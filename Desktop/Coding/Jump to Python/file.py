d79f554bb2234fc3f00d516963a0fb5d8bd45ec4
SeungpilHan <gkstmdvlf2@gmail.com>
2020-04-11 22:19:29 +0900

    file memo

--- Desktop/Coding/Jump to Python/file.py
+++ Desktop/Coding/Jump to Python/file.py
@@ -1,2 +1,17 @@
 f= open("새파일.txt", 'w')
+for i in range(1, 11):
+  data = "%d번째 줄 입니다.\n" % i
+  f.write(data)
+f.close()
+
+f = open("새파일.txt", 'r')
+line = f.readline()
+print(line)
+f.close()
+
+f = open("새파일.txt", 'r')
+while True:
+  line = f.readline()
+  if not line: break
+  print(line)
 f.close()
