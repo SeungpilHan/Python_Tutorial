ac23a5e8a7c42fc38f6753d4a4525e0d892516e5
SeungpilHan <gkstmdvlf2@gmail.com>
2020-04-15 22:24:34 +0900

    __init__

--- Desktop/Coding/Jump to Python/class.py
+++ Desktop/Coding/Jump to Python/class.py
@@ -7,3 +7,14 @@ def adder(num):
 
 print(adder(3))
 print(adder(5))
+
+class Service:
+  secret = "영구는 배꼽이 두개다"
+  def __init__(self, name): #__init__ 인스턴스를 만들 때 항상 실행된다.
+    self.name = name
+  def sum(self, a, b):
+    result = a + b
+    print("%s님 %s + %s = %s입니다." % (self.name, a, b, result))  
+pey = Service("한승필")
+print(pey.secret)
+pey.sum(2, 4)
