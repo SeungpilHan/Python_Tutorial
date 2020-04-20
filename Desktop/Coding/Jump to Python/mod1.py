359ee3b532cf3069f8547910497cd90038fa11c6
SeungpilHan <gkstmdvlf2@gmail.com>
2020-04-20 22:41:36 +0900

    mod2

---
+++ Desktop/Coding/Jump to Python/mod1.py
@@ -0,0 +1,10 @@
+def sum(a, b):
+  return a + b
+
+def safe_sum(a,b):
+  if type(a) != type(b):
+    print("더할 수 있는 것이 아닙니다.")
+    return
+  else:
+    result = sum(a + b)
+  return result
---
+++ Desktop/Coding/Jump to Python/mod2.py
@@ -0,0 +1,4 @@
+import mod1
+
+print(mod1.sum(3,5))
+print(mod1.safe_sum(3,3))
