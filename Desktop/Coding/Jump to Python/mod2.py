447e067bd51108bbc27a89663fe488108515c03a
SeungpilHan <gkstmdvlf2@gmail.com>
2020-04-21 15:29:50 +0900

    mod2

--- Desktop/Coding/Jump to Python/mod2.py
+++ Desktop/Coding/Jump to Python/mod2.py
@@ -1,4 +1,12 @@
-import mod1
-
-print(mod1.sum(3,5))
-print(mod1.safe_sum(3,3))
+PI = 3.141592
+class Math:
+  def solv(self, r):
+    return PI * (r ** 2)
+def sum(a, b):
+  return a + b
+  
+if __name__ == "__main__":
+  print(PI)
+  a = Math()
+  print(a.solv(3))
+  print(sum(PI, 4.4))
