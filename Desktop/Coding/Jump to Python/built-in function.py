d0f83a06c2e8e450d8eec7c2735304a9acd3fffa
SeungpilHan <gkstmdvlf2@gmail.com>
2020-04-28 22:44:17 +0900

    commit

--- Desktop/Coding/Jump to Python/built-in function.py
+++ Desktop/Coding/Jump to Python/built-in function.py
@@ -1,1 +1,23 @@
 print(abs(3))
+print(abs(-3))
+print(abs(-1.3))
+
+print(all([1,2,3]))
+print(all([1, 2, 3, 0]))
+
+print(any([1,2,3,0])) #하나라도 참이면 참
+print(any([0,""]))
+
+print(chr(97))
+print(chr(126))
+
+print(dir([1,2,3]))
+print(dir({'1':'a'}))
+
+print(divmod(7, 3))
+print(divmod(1.3, 0.2))
+
+for i, name in enumerate(['body', 'foo', 'bar']):
+  print(i, name)
+
+  
